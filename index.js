6c225bbc8074fb193132b606323778bc5b4cc55d
Emanoel Carlos <emanoel.carlos@hotmail.com>
2022-03-21 17:43:54 -0300

    scanning all the project structure and exporting all files to .txt format

--- index.js
+++ index.js
@@ -1,22 +1,73 @@
 const fs = require("fs");
+const path = require("path");
+
+let BASE_PATH = "";
+let EXPORTED_BASE_PATH = "";
 
 const main = async () => {
   try {
+    BASE_PATH = process.argv[2];
+    EXPORTED_BASE_PATH = process.argv[3];
 
-    if(!process.argv[2]) {
-      console.log("Especifique o diretório raíz. Exemplo: 'node index.js C:\\Users\\documents\\MeuProjeto' ");
+    if (!BASE_PATH) {
+    // if (!BASE_PATH || !EXPORTED_BASE_PATH) {
+      console.log(
+        "Especifique o diretório raíz do projeto analisado e do projeto exportado. Exemplo: 'node index.js C:\\Users\\documents\\MeuProjeto C:\\Users\\documents\\MeuProjetoExportado'"
+      );
       return;
+    } else {
+      console.log("Scanning file structure ...");
+      const listAllfiles = getListAllfiles(BASE_PATH);
+
+      console.log("Exporting files ...");
+      converFilesToTXT(listAllfiles);
+
+      console.log("Done");
     }
-
-    console.log("ARGV:", process.argv[2]);
-    const BASE_PATH = process.argv[2];
-
-    const arrayOfFiles = fs.readdirSync(BASE_PATH);
-    console.log("arrayOfFiles:");
-    console.log(arrayOfFiles);
   } catch (e) {
     console.log(e);
   }
 };
 
+const getListAllfiles = (dirPath, arrayOfFiles) => {
+  files = fs.readdirSync(dirPath);
+
+  arrayOfFiles = arrayOfFiles || [];
+
+  files.forEach((file) => {
+    let actualPath = dirPath + "/" + file;
+    
+    if (fs.statSync(actualPath).isDirectory()) {
+      createMirroredDirectoryIfItDoesNotExists(actualPath);
+      arrayOfFiles = getListAllfiles(actualPath, arrayOfFiles);
+    } else { 
+      arrayOfFiles.push(path.join(dirPath, "/", file));
+    }
+  });
+
+  return arrayOfFiles;
+};
+
+const createMirroredDirectoryIfItDoesNotExists = (directoryPath) => {
+  let mirroredPath = replaceBasePath(directoryPath);
+  fs.mkdirSync(mirroredPath);
+}
+
+const converFilesToTXT = (listAllfiles) => { 
+  listAllfiles.forEach((file) => {
+    fs.copyFile(file, replaceBasePath(file) + ".txt", err => {
+      if (err) throw err;
+    });
+  })
+};
+
+const replaceBasePath = (filePath) => {
+  return filePath.toString().replace(BASE_PATH, EXPORTED_BASE_PATH);
+}
+
 main();
+
+/**
+ * TODO: mkdirSync(): delete or ignore directory once it already exists
+ */
+
